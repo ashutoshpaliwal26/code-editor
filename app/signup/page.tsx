c632b3d75907e89d98046afc8a93e62eac8aa9ee
Ashutosh Paliwal <112794799+ashutoshpaliwal26@users.noreply.github.com>
2025-07-13 14:26:22 +0530

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,6 +4,7 @@ import SignUpForm from '@/components/Auth/Signup'
 import React from 'react'
 
 const page = () => {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   return (
     <>
         <SignUpForm/>
